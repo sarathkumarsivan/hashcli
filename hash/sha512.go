9513bb6d76b98be102fb67c4ab7a06c76127d260
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-01-03 13:27:26 -0800

    Support SHA-384 cryptographic hash function
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/sha512.go
+++ hash/sha512.go
@@ -6,33 +6,33 @@ import (
 	"encoding/hex"
 )
 
-func SHA512(text string) ([]byte, error) {
+func Sha512(text string) ([]byte, error) {
 	hash := sha512.New()
 	return hashText(hash, text)
 }
 
 func SHA512Hex(text string) (string, error) {
-	hash, err := SHA512(text)
+	hash, err := Sha512(text)
 	return hex.EncodeToString(hash), err
 }
 
 func SHA512Base64StdEnc(text string) (string, error) {
-	hash, err := SHA512(text)
+	hash, err := Sha512(text)
 	return base64.StdEncoding.EncodeToString(hash), err
 }
 
 func SHA512Base64URLEnc(text string) (string, error) {
-	hash, err := SHA512(text)
+	hash, err := Sha512(text)
 	return base64.URLEncoding.EncodeToString(hash), err
 }
 
 func SHA512Base64RawURLEnc(text string) (string, error) {
-	hash, err := SHA512(text)
+	hash, err := Sha512(text)
 	return base64.RawURLEncoding.EncodeToString(hash), err
 }
 
 func SHA512Base64RawStdEnc(text string) (string, error) {
-	hash, err := SHA512(text)
+	hash, err := Sha512(text)
 	return base64.RawStdEncoding.EncodeToString(hash), err
 }
 
