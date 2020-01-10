9ce2075ac0ae5c4547889d2b52d29b9b8401a0f1
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-01-10 14:36:27 -0800

    Support SHA-512 cryptographic hash function
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/md5.go
+++ hash/md5.go
@@ -156,6 +156,8 @@ func Md5PathBase64URLEnc(path string) (string, error) {
 	return base64.URLEncoding.EncodeToString(hash), err
 }
 
+// Md5PathBase64RawURLEnc returns the MD5 checksum of a path in
+// a padded alternate base64 encoding defined in RFC 4648.
 func Md5PathBase64RawURLEnc(path string) (string, error) {
 	hash, err := Md5Path(path)
 	return base64.RawURLEncoding.EncodeToString(hash), err
