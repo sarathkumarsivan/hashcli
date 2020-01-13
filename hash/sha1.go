e70d8c4cba64825d6b72477d5ea90d315f298be5
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-01-13 14:54:00 -0800

    Support SHA-512 cryptographic hash function
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/sha1.go
+++ hash/sha1.go
@@ -115,6 +115,8 @@ func Sha1DirBase64URLEnc(path string) (string, error) {
 	return base64.URLEncoding.EncodeToString(hash), err
 }
 
+// Sha1DirBase64RawURLEnc returns the SHA-1 checksum of a directory in
+// a padded alternate base64 encoding defined in RFC 4648.
 func Sha1DirBase64RawURLEnc(path string) (string, error) {
 	hash, err := Sha1Dir(path)
 	return base64.RawURLEncoding.EncodeToString(hash), err
