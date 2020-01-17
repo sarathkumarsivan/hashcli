28a1658a407b66bf943e09e637e1857dbdb7a416
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-01-16 17:25:29 -0800

    Support SHA-224 cryptographic hash function
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/sha224.go
+++ hash/sha224.go
@@ -81,6 +81,8 @@ func Sha224FileBase64RawURLEnc(path string) (string, error) {
 	return base64.RawURLEncoding.EncodeToString(hash), err
 }
 
+// Sha224FileBase64RawStdEnc returns the SHA-224 checksum of a file in
+// a standard raw, un-padded base64 encoding, as defined in RFC 4648.
 func Sha224FileBase64RawStdEnc(path string) (string, error) {
 	hash, err := Sha224File(path)
 	return base64.RawStdEncoding.EncodeToString(hash), err
