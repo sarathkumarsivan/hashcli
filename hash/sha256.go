3a0cafe93b0c4f161271fcb43662fcf72858e41e
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-01-18 17:35:54 -0800

    Support SHA-256 cryptographic hash function
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/sha256.go
+++ hash/sha256.go
@@ -19,6 +19,8 @@ func Sha256Hex(text string) (string, error) {
 	return hex.EncodeToString(hash), err
 }
 
+// Sha256Base64StdEnc returns the SHA-256 checksum of a text in
+// standard base64 encoding, as defined in RFC 4648.
 func Sha256Base64StdEnc(text string) (string, error) {
 	hash, err := Sha256(text)
 	return base64.StdEncoding.EncodeToString(hash), err
