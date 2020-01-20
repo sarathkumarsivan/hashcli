877163a989149af6f6926437d97d32839edac346
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-01-19 17:42:00 -0800

    Support SHA-256 cryptographic hash function
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/sha256.go
+++ hash/sha256.go
@@ -108,6 +108,8 @@ func Sha256DirBase64StdEnc(path string) (string, error) {
 	return base64.StdEncoding.EncodeToString(hash), err
 }
 
+// Sha256DirBase64URLEnc returns the SHA-256 checksum of a directory in
+// an alternate base64 encoding defined in RFC 4648.
 func Sha256DirBase64URLEnc(path string) (string, error) {
 	hash, err := Sha256Dir(path)
 	return base64.URLEncoding.EncodeToString(hash), err
