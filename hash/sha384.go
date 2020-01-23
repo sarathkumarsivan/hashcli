37d12152c762508bcb74dcb78d03ec1d03396011
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-01-22 17:56:45 -0800

    Support SHA-384 cryptographic hash function
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/sha384.go
+++ hash/sha384.go
@@ -101,6 +101,8 @@ func Sha384DirHex(path string) (string, error) {
 	return hex.EncodeToString(hash), err
 }
 
+// Sha384DirBase64StdEnc returns the SHA-384 checksum of a directory in
+// standard base64 encoding, as defined in RFC 4648.
 func Sha384DirBase64StdEnc(path string) (string, error) {
 	hash, err := Sha384Dir(path)
 	return base64.StdEncoding.EncodeToString(hash), err
