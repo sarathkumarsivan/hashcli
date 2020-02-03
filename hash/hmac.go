7293e5bc9dede7ea0cab9667eb361a957a13738d
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-02-02 19:08:05 -0800

    Support for Hash-based Message Authentication Code (HMAC)
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/hmac.go
+++ hash/hmac.go
@@ -190,6 +190,10 @@ func HmacSha512Base64RawURLEnc(message string, secret string) string {
 	return base64.RawURLEncoding.EncodeToString(bytes)
 }
 
+// HmacSha384 computes a Hash-based Message Authentication Code (HMAC) by using
+// the SHA-384 hash function. The HMAC process mixes a secret key with the message
+// data, hashes the result with the hash function, mixes that hash value with
+// the secret key again, and then applies the hash function a second time.
 func HmacSha384(message string, secret string) []byte {
 	key := []byte(secret)
 	hash := hmac.New(sha512.New384, key)
