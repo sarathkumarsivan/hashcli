0e1eeed8fdacea3e54671e051fb2a7d03a01cb51
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-02-03 19:17:44 -0800

    Support for Hash-based Message Authentication Code (HMAC)
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/hmac.go
+++ hash/hmac.go
@@ -80,6 +80,9 @@ func HmacSha1Hex(message string, secret string) string {
 	return hex.EncodeToString(bytes)
 }
 
+// HmacSha1Base64StdEnc mixes a secret key with the message data, hashes the result with
+// the SHA1 hash function, mixes that hash value with the secret key again, then applies
+// the hash function a second time, and encodes the result using base64 standard encoding.
 func HmacSha1Base64StdEnc(message string, secret string) string {
 	bytes := HmacSha1(message, secret)
 	return base64.StdEncoding.EncodeToString(bytes)
