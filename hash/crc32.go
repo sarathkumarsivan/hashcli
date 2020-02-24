ee37f60ce7d08f7e826830d6071de32d3f1abc9b
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-02-23 21:33:53 -0800

    CRC32 Hashing support
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/crc32.go
+++ hash/crc32.go
@@ -7,8 +7,8 @@ import (
 )
 
 func Crc32(text string) []byte {
-	tablePolynomial := crc32.MakeTable(crc32.Castagnoli)
-	hash := crc32.New(tablePolynomial)
+	table := crc32.MakeTable(crc32.Castagnoli)
+	hash := crc32.New(table)
 	hash.Write([]byte(text))
 	return hash.Sum(nil)[:]
 }
@@ -19,8 +19,14 @@ func Crc32Hex(text string) string {
 	return hex.EncodeToString(Crc32(text))
 }
 
-// Crc32Base64StdEnc returns the MD5 checksum of a text in
+// Crc32Base64StdEnc returns the CRC32 checksum of a text in
 // standard base64 encoding, as defined in RFC 4648.
 func Crc32Base64StdEnc(text string) string {
 	return base64.StdEncoding.EncodeToString(Crc32(text))
+}
+
+// Crc32Base64URLEnc returns the CRC32 checksum of a text in
+// an alternate base64 encoding defined in RFC 4648.
+func Crc32Base64URLEnc(text string) string {
+	return base64.URLEncoding.EncodeToString(Crc32(text))
 }
