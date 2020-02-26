d127e631612a186ca681914c22b9f31163e7f0e1
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-02-25 21:50:37 -0800

    CRC32 Hashing support
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/crc32_test.go
+++ hash/crc32_test.go
@@ -2,12 +2,9 @@ package hash
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestCrc32cHashHex(t *testing.T) {
-	crc32, err := Crc32HashHex("foo")
-	require.NoError(t, err, "Error hashing text to using %s", Crc32Hash)
-	assert.Equal(t, "cfc4ae1d", crc32)
+	assert.Equal(t, "cfc4ae1d", Crc32Hex("foo"))
 }
