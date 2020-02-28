a0f395496e56cf09f2384b9e6e5ea27ff08f540f
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-02-27 22:19:32 -0800

    CRC32 Hashing support
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/crc32_test.go
+++ hash/crc32_test.go
@@ -1,10 +1,12 @@
 package hash
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCrc32cHashHex(t *testing.T) {
 	assert.Equal(t, "cfc4ae1d", Crc32Hex("foo"))
+	assert.Equal(t, "z8SuHQ==", Crc32Base64StdEnc("foo"))
 }
