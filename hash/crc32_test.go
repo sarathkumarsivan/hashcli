2c571fc21e50b55ef417f3dfc7664fc91c0528c6
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-02-27 22:20:51 -0800

    CRC32 Hashing support
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/crc32_test.go
+++ hash/crc32_test.go
@@ -10,4 +10,5 @@ func TestCrc32cHashHex(t *testing.T) {
 	assert.Equal(t, "cfc4ae1d", Crc32Hex("foo"))
 	assert.Equal(t, "z8SuHQ==", Crc32Base64StdEnc("foo"))
 	assert.Equal(t, "z8SuHQ==", Crc32Base64URLEnc("foo"))
+	assert.Equal(t, "z8SuHQ", Crc32Base64RawURLEnc("foo"))
 }
