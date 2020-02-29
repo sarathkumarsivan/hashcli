afd786b234ec776300cb57638a339bd927fa818b
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-02-28 22:29:56 -0800

    CRC32 Hashing support
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/crc32_test.go
+++ hash/crc32_test.go
@@ -1,9 +1,12 @@
 package hash
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCrc32cHashHex(t *testing.T) {
@@ -12,4 +15,18 @@ func TestCrc32cHashHex(t *testing.T) {
 	assert.Equal(t, "z8SuHQ==", Crc32Base64URLEnc("foo"))
 	assert.Equal(t, "z8SuHQ", Crc32Base64RawURLEnc("foo"))
 	assert.Equal(t, "z8SuHQ", Crc32Base64RawStdEnc("foo"))
+}
+
+func TestCrc32HashFile(t *testing.T) {
+	foo, err := ioutil.TempFile("", "foo.*")
+	require.NoError(t, err, "Error creating temporary file")
+	defer func() { _ = os.Remove(foo.Name()) }()
+
+	hash, err := Crc32FileHex(foo.Name())
+	require.NoError(t, err, "Error hashing text to using %s", Crc32Hash)
+	assert.Equal(t, "00000000", hash)
+
+	hash, err = Crc32FileBase64StdEnc(foo.Name())
+	require.NoError(t, err, "Error hashing text to using %s", Crc32Hash)
+	assert.Equal(t, "AAAAAA==", hash)
 }
