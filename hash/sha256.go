4a02eec7449bf5d97d5be72c1b3950ae1e844fba
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-04-12 13:06:01 -0700

    Support SHA-256 cryptographic hash function
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/sha256.go
+++ hash/sha256.go
@@ -6,33 +6,33 @@ import (
 	"encoding/hex"
 )
 
-func SHA256(text string) ([]byte, error) {
+func Sha256(text string) ([]byte, error) {
 	hash := sha256.New()
 	return hashText(hash, text)
 }
 
 func SHA256Hex(text string) (string, error) {
-	hash, err := SHA256(text)
+	hash, err := Sha256(text)
 	return hex.EncodeToString(hash), err
 }
 
 func SHA256Base64StdEnc(text string) (string, error) {
-	hash, err := SHA256(text)
+	hash, err := Sha256(text)
 	return base64.StdEncoding.EncodeToString(hash), err
 }
 
 func SHA256Base64URLEnc(text string) (string, error) {
-	hash, err := SHA256(text)
+	hash, err := Sha256(text)
 	return base64.URLEncoding.EncodeToString(hash), err
 }
 
 func SHA256Base64RawURLEnc(text string) (string, error) {
-	hash, err := SHA256(text)
+	hash, err := Sha256(text)
 	return base64.RawURLEncoding.EncodeToString(hash), err
 }
 
 func SHA256Base64RawStdEnc(text string) (string, error) {
-	hash, err := SHA256(text)
+	hash, err := Sha256(text)
 	return base64.RawStdEncoding.EncodeToString(hash), err
 }
 
