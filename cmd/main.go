2f50203ae49bd15f029687f3d7d1bef5d1034529
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-04-29 22:06:05 -0700

    main changes to hash sha-1

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,13 @@ import (
 	"github.com/sarathkumarsivan/hashcli"
 )
 
+const (
+	HashingAlgorithm = "algorithm"
+	AlgorithmSHA1    = "sha1"
+	AlgorithmMD5     = "md5"
+	FlagText         = "text"
+)
+
 func parseCommandLine() map[string]string {
 	options := make(map[string]string)
 	sha1Hash := flag.NewFlagSet("sha1", flag.ExitOnError)
@@ -19,7 +26,9 @@ func parseCommandLine() map[string]string {
 	}
 
 	switch os.Args[1] {
-	case "sha1":
+	case AlgorithmSHA1:
+		sha1Hash.Parse(os.Args[2:])
+	case AlgorithmMD5:
 		sha1Hash.Parse(os.Args[2:])
 	default:
 		flag.PrintDefaults()
@@ -31,7 +40,7 @@ func parseCommandLine() map[string]string {
 			sha1Hash.PrintDefaults()
 			os.Exit(1)
 		}
-		options["algorithm"] = "sha1"
+		options[HashingAlgorithm] = AlgorithmSHA1
 		options["text"] = *sha1HashText
 	}
 
@@ -40,14 +49,14 @@ func parseCommandLine() map[string]string {
 
 func main() {
 	options := parseCommandLine()
-	if options["algorithm"] == "sha1" {
-		hash := hashcli.GetSHA1Hash(options["text"])
-		fmt.Printf("sha-1 (%s): %x\n", options["text"], hash)
+	if options[HashingAlgorithm] == AlgorithmSHA1 {
+		hash := hashcli.GetSHA1Hash(options[FlagText])
+		fmt.Printf("sha-1 (%s): %x\n", options[FlagText], hash)
 		return
 	}
-	if options["algorithm"] == "md5" {
-		hash := hashcli.GetMD5Hash(options["text"])
-		fmt.Printf("md5 (%s): %x\n", options["text"], hash)
+	if options[HashingAlgorithm] == AlgorithmSHA1 {
+		hash := hashcli.GetMD5Hash(options[FlagText])
+		fmt.Printf("md5 (%s): %x\n", options[FlagText], hash)
 		return
 	}
 }
