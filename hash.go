d80a17cc151205e0301945ad4436ba633bd832fb
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-04-29 21:23:28 -0700

    Add MD5Hash support

--- hash.go
+++ hash.go
@@ -1,7 +1,9 @@
 package hashcli
 
 import (
+	"crypto/md5"
 	"crypto/sha1"
+	"encoding/hex"
 	"io"
 )
 
@@ -9,4 +11,10 @@ func GetSHA1Hash(txt string) string {
 	h := sha1.New()
 	io.WriteString(h, txt)
 	return string(h.Sum(nil))
+}
+
+func GetMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
