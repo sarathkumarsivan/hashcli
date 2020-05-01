27ba459a1cbadb4b59e74fce82852bfc218830c6
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-01 00:21:01 -0700

    Refactor implementation

--- hash.go
+++ hash.go
@@ -12,6 +12,7 @@ import (
 )
 
 const (
+	Algorithm       = "algorithm"
 	AlgorithmMD5    = "md5"
 	AlgorithmSHA1   = "sha1"
 	AlgorithmSHA256 = "sha256"
@@ -20,14 +21,14 @@ const (
 
 func GetHash(text, algorithm string) (string, error) {
 	switch algorithm {
+	case AlgorithmMD5:
+		return getHash(md5.New(), text)
 	case AlgorithmSHA1:
 		return getHash(sha1.New(), text)
 	case AlgorithmSHA256:
 		return getHash(sha256.New(), text)
 	case AlgorithmSHA512:
 		return getHash(sha512.New(), text)
-	case AlgorithmMD5:
-		return getHash(md5.New(), text)
 	default:
 		return "", nil
 	}
@@ -39,24 +40,6 @@ func getHash(h hash.Hash, text string) (string, error) {
 		return "", err
 	}
 	return hex.EncodeToString(h.Sum(nil)), nil
-}
-
-func GetSHA1Hash(text string) string {
-	hash := sha1.New()
-	hash.Write([]byte(text))
-	return hex.EncodeToString(hash.Sum(nil))
-}
-
-func GetSHA256Hash(text string) string {
-	hash := sha256.New()
-	hash.Write([]byte(text))
-	return hex.EncodeToString(hash.Sum(nil))
-}
-
-func GetMD5Hash(text string) string {
-	hash := md5.New()
-	hash.Write([]byte(text))
-	return hex.EncodeToString(hash.Sum(nil))
 }
 
 func GetFileHash(h hash.Hash, filename string) (string, error) {
