597b0188a82e1b39a91955994139d8c93bd7a8cd
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-04-30 22:45:50 -0700

    Include support for GetSHA256Hash

--- hash_test.go
+++ hash_test.go
@@ -1,11 +1,14 @@
 package hashcli
 
 import (
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetSHA1Hash(t *testing.T) {
-	assert.NotEmpty(t, GetSHA1Hash("abc"))
+	assert.Equal(t, "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33", GetSHA1Hash("foo"))
+	assert.Equal(t, "62cdb7020ff920e5aa642c3d4066950dd1f01f4d", GetSHA1Hash("bar"))
+	log.Printf("%v", GetSHA1Hash("bar"))
 }
