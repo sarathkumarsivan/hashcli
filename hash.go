61522b8f11ab4c2e8799a0d8e442c135adffa355
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-04-30 22:59:14 -0700

    Include support for GetSHA256Hash

--- hash.go
+++ hash.go
@@ -5,6 +5,9 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
+	"hash"
+	"io"
+	"os"
 )
 
 func GetSHA1Hash(text string) string {
@@ -23,4 +26,16 @@ func GetMD5Hash(text string) string {
 	hash := md5.New()
 	hash.Write([]byte(text))
 	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func GetFileHash(h hash.Hash, filename string) (string, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
