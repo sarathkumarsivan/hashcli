fb9ee5001a100784b7ae62f911f13ddf31852c97
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-04-30 22:41:04 -0700

    Include support for GetSHA256Hash

--- hash.go
+++ hash.go
@@ -3,11 +3,18 @@ package hashcli
 import (
 	"crypto/md5"
 	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/hex"
 )
 
 func GetSHA1Hash(text string) string {
 	hash := sha1.New()
+	hash.Write([]byte(text))
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func GetSHA256Hash(text string) string {
+	hash := sha256.New()
 	hash.Write([]byte(text))
 	return hex.EncodeToString(hash.Sum(nil))
 }
