1b56547f84463a9e08d02d6fd81b0f0defd837a9
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-01 00:20:46 -0700

    Refactor implementation

--- cmd/main.go
+++ cmd/main.go
@@ -9,17 +9,21 @@ import (
 )
 
 const (
-	HashingAlgorithm = "algorithm"
-	AlgorithmSHA1    = "sha1"
-	AlgorithmSHA256  = "sha256"
-	AlgorithmMD5     = "md5"
-	FlagText         = "text"
+	flagText      = "text"
+	flagTextEmpty = ""
+	flagTextDesc  = "Text or string to Hash"
 )
 
 func parseCommandLine() map[string]string {
 	options := make(map[string]string)
-	sha1Hash := flag.NewFlagSet("sha1", flag.ExitOnError)
-	sha1HashText := sha1Hash.String("text", "", "Text to hash")
+	md5Hash := flag.NewFlagSet(hashcli.AlgorithmMD5, flag.ExitOnError)
+	md5HashText := md5Hash.String(flagText, flagTextEmpty, flagTextEmpty)
+	sha1Hash := flag.NewFlagSet(hashcli.AlgorithmSHA1, flag.ExitOnError)
+	sha1HashText := sha1Hash.String(flagText, flagTextEmpty, flagTextDesc)
+	sha256Hash := flag.NewFlagSet(hashcli.AlgorithmSHA256, flag.ExitOnError)
+	sha256HashText := sha256Hash.String(flagText, flagTextEmpty, flagTextDesc)
+	sha512Hash := flag.NewFlagSet(hashcli.AlgorithmSHA512, flag.ExitOnError)
+	sha512HashText := sha512Hash.String(flagText, flagTextEmpty, flagTextDesc)
 
 	if len(os.Args) < 2 {
 		fmt.Println("unknown command")
@@ -27,42 +31,70 @@ func parseCommandLine() map[string]string {
 	}
 
 	switch os.Args[1] {
-	case AlgorithmSHA1:
+	case hashcli.AlgorithmMD5:
+		md5Hash.Parse(os.Args[2:])
+	case hashcli.AlgorithmSHA1:
 		sha1Hash.Parse(os.Args[2:])
-	case AlgorithmMD5:
-		sha1Hash.Parse(os.Args[2:])
+	case hashcli.AlgorithmSHA256:
+		sha256Hash.Parse(os.Args[2:])
+	case hashcli.AlgorithmSHA512:
+		sha512Hash.Parse(os.Args[2:])
 	default:
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
+	if md5Hash.Parsed() {
+		if *md5HashText == flagTextEmpty {
+			md5Hash.PrintDefaults()
+			os.Exit(1)
+		}
+		options[hashcli.Algorithm] = hashcli.AlgorithmMD5
+		options[flagText] = *md5HashText
+	}
+
 	if sha1Hash.Parsed() {
-		if *sha1HashText == "" {
+		if *sha1HashText == flagTextEmpty {
 			sha1Hash.PrintDefaults()
 			os.Exit(1)
 		}
-		options[HashingAlgorithm] = AlgorithmSHA1
-		options["text"] = *sha1HashText
+		options[hashcli.Algorithm] = hashcli.AlgorithmSHA1
+		options[flagText] = *sha1HashText
+	}
+
+	if sha256Hash.Parsed() {
+		if *sha256HashText == flagTextEmpty {
+			sha256Hash.PrintDefaults()
+			os.Exit(1)
+		}
+		options[hashcli.Algorithm] = hashcli.AlgorithmSHA256
+		options[flagText] = *sha256HashText
+	}
+
+	if sha512Hash.Parsed() {
+		if *sha512HashText == flagTextEmpty {
+			sha512Hash.PrintDefaults()
+			os.Exit(1)
+		}
+		options[hashcli.Algorithm] = hashcli.AlgorithmSHA512
+		options[flagText] = *sha512HashText
 	}
 
 	return options
 }
 
-func main() {
+func run() {
 	options := parseCommandLine()
-	if options[HashingAlgorithm] == AlgorithmSHA1 {
-		hash := hashcli.GetSHA1Hash(options[FlagText])
-		fmt.Printf("sha-1 (%s): %x\n", options[FlagText], hash)
-		return
+	hash, err := hashcli.GetHash(options[flagText], options[hashcli.Algorithm])
+	if err != nil {
+		fmt.Errorf("error hasing text: %s using algorithm %s, error: %s",
+			options[hashcli.Algorithm], options[flagText], err)
+		os.Exit(1)
 	}
-	if options[HashingAlgorithm] == AlgorithmSHA256 {
-		hash := hashcli.GetSHA256Hash(options[FlagText])
-		fmt.Printf("sha-256 (%s): %x\n", options[FlagText], hash)
-		return
-	}
-	if options[HashingAlgorithm] == AlgorithmMD5 {
-		hash := hashcli.GetMD5Hash(options[FlagText])
-		fmt.Printf("md5 (%s): %x\n", options[FlagText], hash)
-		return
-	}
+	fmt.Printf("%s (%s): %s\n", options[hashcli.Algorithm], options[flagText], hash)
+	return
+}
+
+func main() {
+	run()
 }
