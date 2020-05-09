25999c15765bc455cd45744aa691e480128f6113
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-08 22:27:52 -0700

    Support MD5 hashing with multiple encodings

--- hash/md5hash_test.go
+++ hash/md5hash_test.go
@@ -14,4 +14,8 @@ func TestMd5Hash(t *testing.T) {
 	hash, err = md5Base64StdEnc("foo")
 	require.NoError(t, err, "Error hashing text to using %s", MD5Hash)
 	assert.Equal(t, "rL0Y20zC+Fzt72VPzMSk2A==", hash)
+
+	hash, err = md5Base64RawStdEnc("foo")
+	require.NoError(t, err, "Error hashing text to using %s", MD5Hash)
+	assert.Equal(t, "rL0Y20zC+Fzt72VPzMSk2A", hash)
 }
