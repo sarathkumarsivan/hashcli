153c2a7368c11bacf0891ba5acb50c7eb9c62dc1
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-12 23:09:25 -0700

    Hash MD5 File support

--- hash/md5_test.go
+++ hash/md5_test.go
@@ -44,4 +44,7 @@ func TestMD5HashFile(t *testing.T) {
 	require.NoError(t, err, "Error hashing text to using %s", MD5Hash)
 	assert.Equal(t, "1B2M2Y8AsgTpgAmY7PhCfg==", hash)
 
+	hash, err = MD5FileBase64URLEnc(foo.Name())
+	require.NoError(t, err, "Error hashing text to using %s", MD5Hash)
+	assert.Equal(t, "1B2M2Y8AsgTpgAmY7PhCfg==", hash)
 }
