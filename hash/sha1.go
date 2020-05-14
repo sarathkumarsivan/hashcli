afcbda25aed2114ce54b12569d3dd75319098403
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-13 22:04:44 -0700

    Hash SHA1 File support

--- hash/sha1.go
+++ hash/sha1.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"io"
+	"os"
 )
 
 func SHA1(text string) ([]byte, error) {
@@ -36,4 +37,22 @@ func SHA1Base64RawURLEnc(text string) (string, error) {
 func SHA1Base64RawStdEnc(text string) (string, error) {
 	hash, err := SHA1(text)
 	return base64.RawStdEncoding.EncodeToString(hash), err
+}
+
+func SHA1File(path string) ([]byte, error) {
+	hash := sha1.New()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = file.Close() }()
+	if _, err := io.Copy(hash, file); err != nil {
+		return nil, err
+	}
+	return hash.Sum(nil), nil
+}
+
+func SHA1FileHex(path string) (string, error) {
+	hash, err := SHA1File(path)
+	return hex.EncodeToString(hash), err
 }
