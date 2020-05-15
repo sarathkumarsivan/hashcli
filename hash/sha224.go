d317a75fd12ebe96c78c2545c4fc14d6cd45b0d6
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-14 22:56:02 -0700

    File hash support for computing the SHA224 checksum.

--- hash/sha224.go
+++ hash/sha224.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"io"
+	"os"
 )
 
 func SHA224(text string) ([]byte, error) {
@@ -36,4 +37,17 @@ func SHA224Base64RawURLEnc(text string) (string, error) {
 func SHA224Base64RawStdEnc(text string) (string, error) {
 	hash, err := SHA224(text)
 	return base64.RawStdEncoding.EncodeToString(hash), err
+}
+
+func SHA224File(path string) ([]byte, error) {
+	hash := sha256.New224()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = file.Close() }()
+	if _, err := io.Copy(hash, file); err != nil {
+		return nil, err
+	}
+	return hash.Sum(nil), nil
 }
