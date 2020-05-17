0fd26ef920d0a2429f428aa07c3e23b2a686ecdd
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-17 00:02:44 -0700

    File hash support for computing the SHA256 checksum.

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPORT_COVERAGE="coverage.txt"
+REPORT_PROFILE="profile.out"
+
+touch $REPORT_COVERAGE
+
+for dir in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=$REPORT_PROFILE -covermode=atomic "$dir"
+    if [ -f $REPORT_PROFILE ]; then
+        cat $REPORT_PROFILE >> $REPORT_COVERAGE
+        rm $REPORT_PROFILE
+    fi
+done
