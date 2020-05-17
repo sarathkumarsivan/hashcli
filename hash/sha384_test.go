073dc1a2b8f1c4f7cbb0607dfcf6354b75c0c84e
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-17 00:16:30 -0700

    File hash support for computing the SHA384 checksum.

--- hash/sha384_test.go
+++ hash/sha384_test.go
@@ -1,6 +1,8 @@
 package hash
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -27,4 +29,26 @@ func TestSHA384Hash(t *testing.T) {
 	hash, err = SHA384Base64URLEnc("foo")
 	require.NoError(t, err, "Error hashing text to using %s", SHA512Hash)
 	assert.Equal(t, "mMEf_f3VQGdrGhN8saIrKnA1DJpEFx1rEYDGvly7LuP3nVMsih3Z7y6OCOdSo7q7", hash)
+}
+
+func TestSHA384HashFile(t *testing.T) {
+	foo, err := ioutil.TempFile("", "foo.*")
+	require.NoError(t, err, "Error creating temporary file")
+	defer func() { _ = os.Remove(foo.Name()) }()
+
+	hash, err := SHA384FileHex(foo.Name())
+	require.NoError(t, err, "Error hashing text to using %s", SHA384Hash)
+	assert.Equal(t, "38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b", hash)
+
+	hash, err = SHA384FileBase64StdEnc(foo.Name())
+	require.NoError(t, err, "Error hashing text to using %s", SHA384Hash)
+	assert.Equal(t, "OLBgp1GsljhM2TJ+sbHjaiH9txEUvgdDTAzHv2P24donTt6/529l+9Ua0vFImLlb", hash)
+
+	hash, err = SHA384FileBase64URLEnc(foo.Name())
+	require.NoError(t, err, "Error hashing text to using %s", SHA384Hash)
+	assert.Equal(t, "OLBgp1GsljhM2TJ-sbHjaiH9txEUvgdDTAzHv2P24donTt6_529l-9Ua0vFImLlb", hash)
+
+	hash, err = SHA384FileBase64RawURLEnc(foo.Name())
+	require.NoError(t, err, "Error hashing text to using %s", SHA384Hash)
+	assert.Equal(t, "OLBgp1GsljhM2TJ-sbHjaiH9txEUvgdDTAzHv2P24donTt6_529l-9Ua0vFImLlb", hash)
 }
