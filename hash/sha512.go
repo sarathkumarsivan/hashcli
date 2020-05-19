0e91dfd29ac87c39ea71208547aea343b588980e
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-18 22:02:34 -0700

    File hash support for computing the SHA512 checksum.

--- hash/sha512.go
+++ hash/sha512.go
@@ -56,3 +56,8 @@ func SHA512FileHex(path string) (string, error) {
 	hash, err := SHA512File(path)
 	return hex.EncodeToString(hash), err
 }
+
+func SHA512FileBase64StdEnc(path string) (string, error) {
+	hash, err := SHA512File(path)
+	return base64.StdEncoding.EncodeToString(hash), err
+}
