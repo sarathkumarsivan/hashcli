10e616064e6477cdb635db51fb26971bec3eb72c
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-19 00:19:12 -0700

    File hash support for computing the SHA512 checksum.

--- hash/sha512_test.go
+++ hash/sha512_test.go
@@ -43,4 +43,8 @@ func TestSHA512HashFile(t *testing.T) {
 	hash, err = SHA512FileBase64StdEnc(foo.Name())
 	require.NoError(t, err, "Error hashing text to using %s", SHA512Hash)
 	assert.Equal(t, "z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg/SpIdNs6c5H0NE8XYXysP+DGNKHfuwvY7kxvUdBeoGlODJ6+SfaPg==", hash)
+
+	hash, err = SHA512FileBase64URLEnc(foo.Name())
+	require.NoError(t, err, "Error hashing text to using %s", SHA512Hash)
+	assert.Equal(t, "OLBgp1GsljhM2TJ-sbHjaiH9txEUvgdDTAzHv2P24donTt6_529l-9Ua0vFImLlb", hash)
 }
