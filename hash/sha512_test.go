cd09e3258c4c76798c4deff42c03c54e030ba8ed
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-18 22:06:30 -0700

    File hash support for computing the SHA512 checksum.

--- hash/sha512_test.go
+++ hash/sha512_test.go
@@ -35,4 +35,9 @@ func TestSHA512HashFile(t *testing.T) {
 	foo, err := ioutil.TempFile("", "foo.*")
 	require.NoError(t, err, "Error creating temporary file")
 	defer func() { _ = os.Remove(foo.Name()) }()
+
+	hash, err := SHA512FileHex(foo.Name())
+	require.NoError(t, err, "Error hashing text to using %s", SHA512Hash)
+	assert.Equal(t, "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e", hash)
+
 }
