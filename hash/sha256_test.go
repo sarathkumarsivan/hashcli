23696cfeaf44e6561ca5eb4fe55101c26e7bc825
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-27 23:31:29 -0700

    SHA256 Hash directory support

--- hash/sha256_test.go
+++ hash/sha256_test.go
@@ -61,4 +61,11 @@ func TestSHA256HashDir(t *testing.T) {
 	dir, err := ioutil.TempDir("", "qux")
 	require.NoError(t, err, "Error creating temporary directory")
 	defer os.Remove(dir)
+
+	foo, err := ioutil.TempFile(dir, "foo.*")
+	require.NoError(t, err, "Error creating temporary file")
+	_, err = foo.WriteString("foo")
+	require.NoError(t, err, "Error writing to temporary file")
+	defer os.Remove(foo.Name())
+
 }
