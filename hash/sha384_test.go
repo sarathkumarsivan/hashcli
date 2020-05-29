351c21756079b84a9717c4a2e2c5826622a80ea6
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-28 21:14:25 -0700

    SHA256 Hash directory support

--- hash/sha384_test.go
+++ hash/sha384_test.go
@@ -67,4 +67,14 @@ func TestSHA384HashDir(t *testing.T) {
 	_, err = foo.WriteString("foo")
 	require.NoError(t, err, "Error writing to temporary file")
 	defer os.Remove(foo.Name())
+
+	bar, err := ioutil.TempFile(dir, "bar.*")
+	require.NoError(t, err, "Error creating temporary file")
+	_, err = bar.WriteString("bar")
+	require.NoError(t, err, "Error writing to temporary file")
+	defer os.Remove(bar.Name())
+
+	hash, err := SHA384DirHex(dir)
+	require.NoError(t, err, "Error hashing dir to using %s", SHA384Hash)
+	assert.NotEmpty(t, hash)
 }
