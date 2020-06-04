758e885bbeb5a30adf125d0115e1743a0ba2976c
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-03 22:58:30 -0700

    Determine files or directories before hashing

--- hash/sha1_test.go
+++ hash/sha1_test.go
@@ -110,6 +110,10 @@ func TestSHA1HashPath(t *testing.T) {
 	require.NoError(t, err, "Error hashing text to using %s", SHA1Hash)
 	assert.NotEmpty(t, hash)
 
+	hash, err = SHA1PathHex(dir)
+	require.NoError(t, err, "Error hashing text to using %s", SHA1Hash)
+	assert.NotEmpty(t, hash)
+
 	hash, err = SHA1PathBase64StdEnc(foo.Name())
 	require.NoError(t, err, "Error hashing text to using %s", SHA1Hash)
 	assert.NotEmpty(t, hash)
