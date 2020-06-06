4531f9e8f81ac137ad2380cd3ca1e15844ecbd31
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-05 19:15:11 -0700

    Determine files or directories before hashing

--- hash/sha224_test.go
+++ hash/sha224_test.go
@@ -114,4 +114,8 @@ func TestSHA22HashPath(t *testing.T) {
 	require.NoError(t, err, "Error hashing text to using %s", SHA224Hash)
 	assert.NotEmpty(t, hash)
 
+	hash, err = SHA224PathBase64StdEnc(dir)
+	require.NoError(t, err, "Error hashing text to using %s", SHA224Hash)
+	assert.NotEmpty(t, hash)
+
 }
