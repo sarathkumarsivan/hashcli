9eaeee23abd950b102a0e162641c8a26558686da
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-06 23:23:33 -0700

    Determine files or directories before hashing

--- hash/sha256.go
+++ hash/sha256.go
@@ -115,3 +115,8 @@ func SHA256PathBase64URLEnc(path string) (string, error) {
 	hash, err := SHA256Path(path)
 	return base64.URLEncoding.EncodeToString(hash), err
 }
+
+func SHA256PathBase64RawURLEnc(path string) (string, error) {
+	hash, err := SHA256Path(path)
+	return base64.RawURLEncoding.EncodeToString(hash), err
+}
