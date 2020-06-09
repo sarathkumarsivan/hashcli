4ec7e934fc6cbc3c3a5071aa651b122e021836f5
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-08 23:20:42 -0700

    Determine files or directories before hashing

--- hash/sha384_test.go
+++ hash/sha384_test.go
@@ -113,4 +113,8 @@ func TestSHA384HashPath(t *testing.T) {
 	hash, err = SHA384PathBase64StdEnc(dir)
 	require.NoError(t, err, "Error hashing text to using %s", SHA384Hash)
 	assert.NotEmpty(t, hash)
+
+	hash, err = SHA384PathBase64URLEnc(dir)
+	require.NoError(t, err, "Error hashing text to using %s", SHA384Hash)
+	assert.NotEmpty(t, hash)
 }
