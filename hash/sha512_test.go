3fd04d9cc628fc0839b86392c10b42fb3c8eae17
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-10 23:16:37 -0700

    Determine files or directories before hashing

--- hash/sha512_test.go
+++ hash/sha512_test.go
@@ -113,4 +113,9 @@ func TestSHA512HashPath(t *testing.T) {
 	hash, err = SHA512PathBase64StdEnc(dir)
 	require.NoError(t, err, "Error hashing text to using %s", SHA512Hash)
 	assert.NotEmpty(t, hash)
+
+	hash, err = SHA512PathBase64URLEnc(dir)
+	require.NoError(t, err, "Error hashing text to using %s", SHA512Hash)
+	assert.NotEmpty(t, hash)
+
 }
