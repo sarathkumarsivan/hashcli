a99f19385b31293c962cb539dcccb2ad8afe2a9d
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-13 03:13:19 -0700

    Support Keyed-Hash Message Authentication Code (HMAC)
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/sha1.go
+++ hash/sha1.go
@@ -31,7 +31,7 @@ func Sha1Base64RawURLEnc(text string) (string, error) {
 	return base64.RawURLEncoding.EncodeToString(hash), err
 }
 
-func SHA1Base64RawStdEnc(text string) (string, error) {
+func Sha1Base64RawStdEnc(text string) (string, error) {
 	hash, err := Sha1(text)
 	return base64.RawStdEncoding.EncodeToString(hash), err
 }
--- hash/sha1_test.go
+++ hash/sha1_test.go
@@ -18,7 +18,7 @@ func TestSHA1Hash(t *testing.T) {
 	require.NoError(t, err, "Error hashing text to using %s", SHA1Hash)
 	assert.Equal(t, "C+7Hteo/D9vJXQ3UfzxbwnXaijM=", hash)
 
-	hash, err = SHA1Base64RawStdEnc("foo")
+	hash, err = Sha1Base64RawStdEnc("foo")
 	require.NoError(t, err, "Error hashing text to using %s", SHA1Hash)
 	assert.Equal(t, "C+7Hteo/D9vJXQ3UfzxbwnXaijM", hash)
 
