2ebcca6cecfb35ce335c9b88d7e641a081f87b03
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-15 03:02:38 -0700

    Support Keyed-Hash Message Authentication Code (HMAC)
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/md5.go
+++ hash/md5.go
@@ -56,7 +56,7 @@ func Md5FileBase64URLEnc(path string) (string, error) {
 	return base64.URLEncoding.EncodeToString(hash), err
 }
 
-func MD5FileBase64RawURLEnc(path string) (string, error) {
+func Md5FileBase64RawURLEnc(path string) (string, error) {
 	hash, err := Md5File(path)
 	return base64.RawURLEncoding.EncodeToString(hash), err
 }
--- hash/md5_test.go
+++ hash/md5_test.go
@@ -48,7 +48,7 @@ func TestMD5HashFile(t *testing.T) {
 	require.NoError(t, err, "Error hashing text to using %s", MD5Hash)
 	assert.Equal(t, "1B2M2Y8AsgTpgAmY7PhCfg==", hash)
 
-	hash, err = MD5FileBase64RawURLEnc(foo.Name())
+	hash, err = Md5FileBase64RawURLEnc(foo.Name())
 	require.NoError(t, err, "Error hashing text to using %s", MD5Hash)
 	assert.Equal(t, "1B2M2Y8AsgTpgAmY7PhCfg", hash)
 
