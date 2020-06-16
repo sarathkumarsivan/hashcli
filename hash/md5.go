3a79b054a54bc2da42370d702089dd37675e62a2
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-16 02:57:59 -0700

    Support Keyed-Hash Message Authentication Code (HMAC)
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/md5.go
+++ hash/md5.go
@@ -6,33 +6,33 @@ import (
 	"encoding/hex"
 )
 
-func MD5(text string) ([]byte, error) {
+func Md5(text string) ([]byte, error) {
 	hash := md5.New()
 	return hashText(hash, text)
 }
 
 func MD5Hex(text string) (string, error) {
-	hash, err := MD5(text)
+	hash, err := Md5(text)
 	return hex.EncodeToString(hash), err
 }
 
 func MD5Base64StdEnc(text string) (string, error) {
-	hash, err := MD5(text)
+	hash, err := Md5(text)
 	return base64.StdEncoding.EncodeToString(hash), err
 }
 
 func MD5Base64URLEnc(text string) (string, error) {
-	hash, err := MD5(text)
+	hash, err := Md5(text)
 	return base64.URLEncoding.EncodeToString(hash), err
 }
 
 func MD5Base64RawURLEnc(text string) (string, error) {
-	hash, err := MD5(text)
+	hash, err := Md5(text)
 	return base64.RawURLEncoding.EncodeToString(hash), err
 }
 
 func MD5Base64RawStdEnc(text string) (string, error) {
-	hash, err := MD5(text)
+	hash, err := Md5(text)
 	return base64.RawStdEncoding.EncodeToString(hash), err
 }
 
