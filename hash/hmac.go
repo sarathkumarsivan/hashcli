f25e1bca6135d1aac6799b10b9c8e7d0867bc0ca
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-19 02:37:34 -0700

    Support Keyed-Hash Message Authentication Code (HMAC)

--- hash/hmac.go
+++ hash/hmac.go
@@ -74,7 +74,7 @@ func Hmac1Base64RawURLEnc(message string, secret string) string {
 	return base64.RawURLEncoding.EncodeToString(bytes)
 }
 
-func HMAC256(message string, secret string) []byte {
+func Hmac256(message string, secret string) []byte {
 	key := []byte(secret)
 	hash := hmac.New(sha256.New, key)
 	hash.Write([]byte(message))
@@ -82,27 +82,27 @@ func HMAC256(message string, secret string) []byte {
 }
 
 func HMAC256Hex(message string, secret string) string {
-	bytes := HMAC256(message, secret)
+	bytes := Hmac256(message, secret)
 	return hex.EncodeToString(bytes)
 }
 
 func HMAC256Base64StdEnc(message string, secret string) string {
-	bytes := HMAC256(message, secret)
+	bytes := Hmac256(message, secret)
 	return base64.StdEncoding.EncodeToString(bytes)
 }
 
 func HMAC256Base64RawStdEnc(message string, secret string) string {
-	bytes := HMAC256(message, secret)
+	bytes := Hmac256(message, secret)
 	return base64.RawStdEncoding.EncodeToString(bytes)
 }
 
 func HMAC256Base64URLEnc(message string, secret string) string {
-	bytes := HMAC256(message, secret)
+	bytes := Hmac256(message, secret)
 	return base64.URLEncoding.EncodeToString(bytes)
 }
 
 func HMAC256Base64RawURLEnc(message string, secret string) string {
-	bytes := HMAC256(message, secret)
+	bytes := Hmac256(message, secret)
 	return base64.RawURLEncoding.EncodeToString(bytes)
 }
 
