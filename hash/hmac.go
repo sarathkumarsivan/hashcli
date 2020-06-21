4d526154fe4776b20c13e4c0ccf69947463605c7
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-21 02:26:00 -0700

    Support Keyed-Hash Message Authentication Code (HMAC)

--- hash/hmac.go
+++ hash/hmac.go
@@ -32,6 +32,11 @@ func HmacMd5Base64RawStdEnc(message string, secret string) string {
 	return base64.RawStdEncoding.EncodeToString(bytes)
 }
 
+func HmacMd5Base64URLEnc(message string, secret string) string {
+	bytes := HmacMd5(message, secret)
+	return base64.URLEncoding.EncodeToString(bytes)
+}
+
 func HMAC1(message string, secret string) []byte {
 	key := []byte(secret)
 	hash := hmac.New(sha1.New, key)
