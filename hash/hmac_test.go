35babc31fc3ededed403a9daec4c0aa0a92f005c
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-23 02:01:04 -0700

    Support Keyed-Hash Message Authentication Code (HMAC)

--- hash/hmac_test.go
+++ hash/hmac_test.go
@@ -26,4 +26,5 @@ func TestHMAC(t *testing.T) {
 	assert.Equal(t, "JCV9chBYKmXHMexVFZyBhMwkwCSJRT5YWH9x9EwjotYbS3IVSonRey1JRIqEUuoGb0_FaivOrUXAiFcv_M2z2A", HMAC512Base64RawURLEnc("foo", "bar"))
 
 	assert.Equal(t, "1d9070d07cb7746e0664cccc6cec1fa996dc7f46368982acfa2095ee8d73fe25b5b6e32279900cdb0fd372a3654e41c5", HMAC384Hex("foo", "bar"))
+	assert.Equal(t, "HZBw0Hy3dG4GZMzMbOwfqZbcf0Y2iYKs+iCV7o1z/iW1tuMieZAM2w/TcqNlTkHF", HMAC384Base64StdEnc("foo", "bar"))
 }
