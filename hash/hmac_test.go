53bace3f060121056dedc6cec2bb7d30e72b51d3
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-27 01:25:27 -0700

    Support Keyed-Hash Message Authentication Code (HMAC)

---
+++ hash/hmac_test.go
@@ -0,0 +1,12 @@
+package hash
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHMAC(t *testing.T) {
+	assert.Equal(t, "147933218aaabc0b8b10a2b3a5c34684c8d94341bcf10a4736dc7270f7741851", HMAC256Hex("foo", "bar"))
+	assert.Equal(t, "FHkzIYqqvAuLEKKzpcNGhMjZQ0G88QpHNtxycPd0GFE=", HMAC256Base64StdEnc("foo", "bar"))
+}
