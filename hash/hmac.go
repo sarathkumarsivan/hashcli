dbfe3aadf4810790f926aeb7a3066982df512757
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-27 01:13:39 -0700

    Support Keyed-Hash Message Authentication Code (HMAC)

--- hash/hmac.go
+++ hash/hmac.go
@@ -3,12 +3,11 @@ package hash
 import (
 	"crypto/hmac"
 	"crypto/sha256"
-	"encoding/base64"
 )
 
-func Hmac256(message string, secret string) string {
+func HMAC256(message string, secret string) []byte {
 	key := []byte(secret)
 	hash := hmac.New(sha256.New, key)
 	hash.Write([]byte(message))
-	return base64.StdEncoding.EncodeToString(hash.Sum(nil))
+	return hash.Sum(nil)
 }
