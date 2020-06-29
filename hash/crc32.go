1460e029c0aaf5b4c2c12711d6e7754394d828ad
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-06-28 21:28:18 -0700

    CRC32 Hashing support
    Signed-off-by: Sarath Kumar Sivan <sarathkumarsivan@gmail.com>

--- hash/crc32.go
+++ hash/crc32.go
@@ -3,22 +3,18 @@ package hash
 import (
 	"encoding/hex"
 	"hash/crc32"
-	"io"
-	"strings"
 )
 
-func Crc32(text string) ([]byte, error) {
+func Crc32(text string) []byte {
 	tablePolynomial := crc32.MakeTable(crc32.Castagnoli)
 	hash := crc32.New(tablePolynomial)
-	if _, err := io.Copy(hash, strings.NewReader(text)); err != nil {
-		return nil, err
-	}
-	return hash.Sum(nil)[:], nil
+	hash.Write([]byte(text))
+	return hash.Sum(nil)[:]
 }
 
 // Crc32HashHex returns the CRC32 checksum of a text in
 // hexadecimal encoding format.
-func Crc32HashHex(text string) (string, error) {
-	hash, err := Crc32(text)
-	return hex.EncodeToString(hash), err
+func Crc32HashHex(text string) string {
+	hash := Crc32(text)
+	return hex.EncodeToString(hash)
 }
